c61fe13c0065f7cba4654dc0bfcde59e926fda58
Neder <nedersimoes@gmail.com>
2022-05-18 16:49:48 -0400

    final project miniblog

--- 10_MINI_BLOG/miniblog/src/App.js
+++ 10_MINI_BLOG/miniblog/src/App.js
@@ -17,6 +17,9 @@ import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import CreatePost from "./pages/CreatePost/CreatePost";
 import Dashboard from "./pages/Dashboard/Dashboard";
+import Search from "./pages/Search/Search";
+import Post from "./pages/Post/Post";
+import EditPost from "./pages/EditPost/EditPost";
 
 function App() {
   const [user, setUser] = useState(undefined);
@@ -41,6 +44,8 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
+              <Route path="/search" element={<Search />} />
+              <Route path="/posts/:id" element={<Post />} />
               <Route
                 path="/login"
                 element={!user ? <Login /> : <Navigate to="/" />}
@@ -48,6 +53,10 @@ function App() {
               <Route
                 path="/register"
                 element={!user ? <Register /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/posts/edit/:id"
+                element={user ? <EditPost /> : <Navigate to="/" />}
               />
               <Route
                 path="/posts/create"
---
+++ 10_MINI_BLOG/miniblog/src/pages/Search/Search.js
@@ -0,0 +1,31 @@
+import { useQuery } from "../../hooks/useQuery";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+import { Link } from "react-router-dom";
+import PostDetail from "../../components/PostDetail";
+import styles from "./Search.module.css";
+
+const Search = () => {
+  const query = useQuery();
+  const search = query.get("q");
+
+  const { documents: posts } = useFetchDocuments("posts", search);
+
+  return (
+    <div className={styles.search_container}>
+      <h2>Search</h2>
+      <div>
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Não foram encontrados posts a partir da sua busca...</p>
+            <Link to="/" className="btn btn-dark">
+              Voltar
+            </Link>
+          </div>
+        )}
+        {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
+      </div>
+    </div>
+  );
+};
+
+export default Search;
